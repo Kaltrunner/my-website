b8c83e3b5f8a0930a791736e40051af22f915fca
Erik <erikrussells@gmail.com>
2022-07-19 18:20:27 -0400

    Prett much done with the html and css, now just working on Javascript.

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-
-document.querySelector("body").appendChild(h2);
+element.addEventListener("click", )
