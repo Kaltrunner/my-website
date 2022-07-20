20b013e7ab8f12d6159f68e9dca706d3897ef585
Erik <erikrussells@gmail.com>
2022-07-20 13:26:02 -0400

    almost done!

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-element.addEventListener("click", )
+let myImage = document.querySelectorAll('img');
+
+myImage.onclick = function(){
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === "https://f4.bcbits.com/img/0025478606_10.jpg")
+    {
+        myImage.setAttribute('src', "https://i1.sndcdn.com/artworks-000466843089-ztjz41-t240x240.jpg");
+    }
+    else
+    {
+        myImage.setAttribute('src', "https://f4.bcbits.com/img/0025478606_10.jpg");
+    }
+}
